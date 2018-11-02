ef7ae3bfff181bad133f352d9e8598ffc3ffb02a
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-02 22:04:23 +0500

    Update Bye.java

--- Chat-Bot/src/answers/Bye.java
+++ Chat-Bot/src/answers/Bye.java
@@ -2,11 +2,10 @@ package answers;
 
 import bot.TopicConversation;
 import bot.AnswerData;
+import bot.BaseTopicConversation;
 import bot.InputData;
-import bot.Mood;
 
-public class Bye implements TopicConversation {
-
+public class Bye extends BaseTopicConversation implements TopicConversation {
 	public static String[] angryAnswers = { "Прощай, надеюсь больше не увидимся", "Пока, лучше больше не пиши мне",
 			"Ну наконец-то, а то я так устал от общения с тобой", "Пока" };
 	public static String[] neutralAnswers = { "Пока, дружище. Рад был тебя видеть",
@@ -14,15 +13,6 @@ public class Bye implements TopicConversation {
 	public static String[] cheerfulAnswers = { "Иди, но обещай вернуться:D", "Чао)))", "До скорой встречи, дружище)" };
 
 	public AnswerData getAnswerData(InputData input) {
-		if (input.currentAttitude.getFriendliness() == Mood.Angry) {
-			int random = 0 + (int) (Math.random() * angryAnswers.length);
-			return new AnswerData(angryAnswers[random], false);
-		} else if (input.currentAttitude.getFriendliness() == Mood.Neutral) {
-			int random = 0 + (int) (Math.random() * neutralAnswers.length);
-			return new AnswerData(neutralAnswers[random], false);
-		} else {
-			int random = 0 + (int) (Math.random() * cheerfulAnswers.length);
-			return new AnswerData(cheerfulAnswers[random], false);
-		}
+		return super.getAnswerData(input, angryAnswers, neutralAnswers, cheerfulAnswers);
 	}
 }
