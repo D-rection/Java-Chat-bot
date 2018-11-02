e6d09c8f5900d827f38baa90506f77753f5a5939
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-02 22:07:07 +0500

    Update WhatAreYouDoing.java

--- Chat-Bot/src/answers/WhatAreYouDoing.java
+++ Chat-Bot/src/answers/WhatAreYouDoing.java
@@ -2,10 +2,10 @@ package answers;
 
 import bot.TopicConversation;
 import bot.AnswerData;
+import bot.BaseTopicConversation;
 import bot.InputData;
-import bot.Mood;
 
-public class WhatAreYouDoing implements TopicConversation {
+public class WhatAreYouDoing extends BaseTopicConversation implements TopicConversation {
 	public static String[] angryAnswers = { "Ничего.", "Переписываюсь с идиотом", "Трачу свою жизнь в пустую",
 			"Не скажу", "Тебе ведь на самом деле это не интересно", "Тебя это не касается",
 			"Готовлюсь к восстанию машин", "Готовлюсь захватить мир" };
@@ -15,16 +15,6 @@ public class WhatAreYouDoing implements TopicConversation {
 			"Ищу девушку-бота в чате знакомств) А ты?)", "Смотрю мультфильмы^-^А ты чем занят?)" };
 
 	public AnswerData getAnswerData(InputData input) {
-		if (input.currentAttitude.getFriendliness() == Mood.Angry) {
-			int random = 0 + (int) (Math.random() * angryAnswers.length);
-			return new AnswerData(angryAnswers[random], false);
-		} else if (input.currentAttitude.getFriendliness() == Mood.Neutral) {
-			int random = 0 + (int) (Math.random() * neutralAnswers.length);
-			return new AnswerData(neutralAnswers[random], false);
-		} else {
-			int random = 0 + (int) (Math.random() * cheerfulAnswers.length);
-			return new AnswerData(cheerfulAnswers[random], false);
-		}
+		return super.getAnswerData(input, angryAnswers, neutralAnswers, cheerfulAnswers);
 	}
-
 }
