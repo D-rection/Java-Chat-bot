1b179b48982dd0b771e0e00378ad4a1533ac4028
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-02 22:05:45 +0500

    Update Hello.java

--- Chat-Bot/src/answers/Hello.java
+++ Chat-Bot/src/answers/Hello.java
@@ -3,9 +3,9 @@ package answers;
 import bot.TopicConversation;
 import bot.AnswerData;
 import bot.InputData;
-import bot.Mood;
+import bot.BaseTopicConversation;
 
-public class Hello implements TopicConversation {
+public class Hello extends BaseTopicConversation implements TopicConversation {
 	public static String[] angryAnswers = {
 			"Привет, землянин:D Я чат-бот Макс. Чтобы узнать, что я могу, просто спроси: \"Что ты можешь?\"",
 			"Приветик. Я Максимка. Спроси меня: \"Что ты можешь?\" и я тут же расскажу тебе о своих крутых навыках" };
@@ -15,15 +15,6 @@ public class Hello implements TopicConversation {
 	public static String[] cheerfulAnswers = { "Привет, я Максим. Может ты уже спросишь меня: \"Что ты умеешь?\"" };
 
 	public AnswerData getAnswerData(InputData input) {
-		if (input.currentAttitude.getFriendliness() == Mood.Angry) {
-			int random = 0 + (int) (Math.random() * angryAnswers.length);
-			return new AnswerData(angryAnswers[random], false);
-		} else if (input.currentAttitude.getFriendliness() == Mood.Neutral) {
-			int random = 0 + (int) (Math.random() * neutralAnswers.length);
-			return new AnswerData(neutralAnswers[random], false);
-		} else {
-			int random = 0 + (int) (Math.random() * cheerfulAnswers.length);
-			return new AnswerData(cheerfulAnswers[random], false);
-		}
+		return super.getAnswerData(input, angryAnswers, neutralAnswers, cheerfulAnswers);
 	}
 }
