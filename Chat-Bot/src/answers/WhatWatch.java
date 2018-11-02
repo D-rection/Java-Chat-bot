7b7f89d8b3b67ded6a6ad01a35240c8bc435a76e
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-02 22:08:54 +0500

    Update WhatWatch.java

--- Chat-Bot/src/answers/WhatWatch.java
+++ Chat-Bot/src/answers/WhatWatch.java
@@ -2,21 +2,15 @@ package answers;
 
 import bot.TopicConversation;
 import bot.AnswerData;
+import bot.BaseTopicConversation;
 import bot.InputData;
-import bot.Mood;
 
-public class WhatWatch implements TopicConversation{ 
+public class WhatWatch extends BaseTopicConversation implements TopicConversation {
 	public static String[] angryAnswers = { "Сам ищи", "Я не хочу тебе помогать", "Смотри, что хочешь", "Лень искать",
-	"Давай как-нибудь сам" };
+			"Давай как-нибудь сам" };
 	public static String[] neutralAnswers = { "Что ты хочешь посмотреть: фильм, мультфильм или сериал?" };
-	
+
 	public AnswerData getAnswerData(InputData input) {
-		if (input.currentAttitude.getFriendliness() == Mood.Angry) {
-			int random = 0 + (int) (Math.random() * angryAnswers.length);
-			return new AnswerData(angryAnswers[random], false);
-		} else {
-			int random = 0 + (int) (Math.random() * neutralAnswers.length);
-			return new AnswerData(neutralAnswers[random], false);
-		}
+		return super.getAnswerData(input, angryAnswers, neutralAnswers, neutralAnswers);
 	}
 }
