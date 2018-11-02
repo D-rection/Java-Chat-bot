4d29ee72e33a6499dc3db2a2bee3cf4c0a08830f
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-02 22:08:05 +0500

    Update WhatDay.java

--- Chat-Bot/src/answers/WhatDay.java
+++ Chat-Bot/src/answers/WhatDay.java
@@ -1,22 +1,12 @@
 package answers;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import bot.TopicConversation;
 import bot.AnswerData;
+import bot.BaseTopicConversation;
 import bot.InputData;
-import bot.Mood;
 
-public class WhatDay implements TopicConversation {
-	public static String[] angryAnswers = { "У тебя что телефона нет?", "В углу экрана посмотри" };
-
+public class WhatDay extends BaseTopicConversation implements TopicConversation {
 	public AnswerData getAnswerData(InputData input) {
-		Date date = new Date();
-		if (input.currentAttitude.getFriendliness() == Mood.Angry) {
-			int random = 0 + (int) (Math.random() * angryAnswers.length);
-			return new AnswerData(angryAnswers[random], false);
-		} else {
-			return new AnswerData(new SimpleDateFormat("'Сегодня' EE dd.MM.yyyy").format(date), false);
-		}
+		return super.getAnswerDataWithDate(input, "'Сегодня' EE dd.MM.yyyy");
 	}
 }
