d47851b13832feb39314e71259e1cfb264f26c41
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-03 00:24:15 +0500

    Update CartoonTest.java

--- Chat-Bot/src/test/CartoonTest.java
+++ Chat-Bot/src/test/CartoonTest.java
@@ -8,37 +8,6 @@ import bot.Attitude;
 import bot.InputData;
 
 class CartoonTest {
-
-	@Test
-	void attitudeAngryTest() {
-		Attitude a = new Attitude();
-		for (int i = 0; i < 5; i++)
-			a.decreaseFriendliness();
-		Cartoon cartoon = new Cartoon();
-		String answer = cartoon.getAnswerData(new InputData(a, null)).getAnswer();
-		assertTrue(Arrays.asList(Cartoon.angryAnswers).contains(answer));
-	}
-
-	@Test
-	void attitudeNeutralTest() {
-		Attitude a = new Attitude();
-		Cartoon cartoon = new Cartoon();
-		String answer = cartoon.getAnswerData(new InputData(a, null, null)).getAnswer();
-		String[] arrAnswers = answer.split("\"");
-		assertTrue(Arrays.asList(Cartoon.neutralAnswers).contains(arrAnswers[0]));
-	}
-
-	@Test
-	void attitudeCheerfulTest() {
-		Attitude a = new Attitude();
-		for (int i = 0; i < 5; i++)
-			a.increasedFriendliness();
-		Cartoon cartoon = new Cartoon();
-		String answer = cartoon.getAnswerData(new InputData(a, null, null)).getAnswer();
-		String[] arrAnswers = answer.split("\"");
-		assertTrue(Arrays.asList(Cartoon.cheerfulAnswers).contains(arrAnswers[0]));
-	}
-
 	@Test
 	void cartoonTest() {
 		Attitude a = new Attitude();
