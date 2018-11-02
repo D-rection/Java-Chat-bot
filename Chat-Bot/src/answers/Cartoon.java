f0bd8b972b77fe731ed5500da8cbddd4b5dc6a79
TanyaFox <37245552+tatyana33993@users.noreply.github.com>
2018-11-02 22:14:43 +0500

    Update Cartoon.java

--- Chat-Bot/src/answers/Cartoon.java
+++ Chat-Bot/src/answers/Cartoon.java
@@ -2,7 +2,6 @@ package answers;
 
 import bot.TopicConversation;
 import bot.AnswerData;
-import bot.BaseTopicConversation;
 import bot.InputData;
 
 public class Cartoon extends BaseTopicConversation implements TopicConversation {
