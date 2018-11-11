39f51f6ec017b52fc188b3c80dd1cd032fc42406
Yury Pliner <yury.pliner@gmail.com>
2018-11-11 13:25:32 +0500

    Замечания по ревью

--- Chat-Bot/src/bot/NameTopics.java
+++ Chat-Bot/src/bot/NameTopics.java
@@ -1,5 +1,6 @@
 package bot;
 
+//TODO Такое ощущение, что после того, как вы выкинете Memory, этот enum перестанет быть нужен
 public enum NameTopics {
 	Hello, WhatCan, HowAreYou, WhatAreYouDoing, WhatDay,
 	WhatTime, Bye, WhatWatch, Cartoon, Film, Series, TownsGame,
